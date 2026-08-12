e3b04b530a64997ceb72960547515b931c096bc5
MNP-GH-411531 <411531@manappuram.com>
2026-08-12 16:31:10 +0530

    Added play card response dto

---
+++ UnoNoMercy.GameEngine/DTOs/PlayCardResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace UnoNoMercy.GameEngine.DTOs
+{
+    public class PlayCardResponse
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+    }
+}

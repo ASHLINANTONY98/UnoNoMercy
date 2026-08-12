f4dfed04c104321bd26d03a7c8d88f6966ec7a89
MNP-GH-411531 <411531@manappuram.com>
2026-08-12 12:43:58 +0530

    Added game state endpoint for retrieving current match snapshot

---
+++ UnoNoMercy.Api/Controllers/GameController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnoNoMercy.Api.Services;
+using UnoNoMercy.GameEngine.Enums;
+using UnoNoMercy.GameEngine.Models;
+using UnoNoMercy.GameEngine.Services;
+
+namespace UnoNoMercy.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GameController : ControllerBase
+{
+    private readonly GameManager _gameManager;
+
+    public GameController(GameManager gameManager)
+    {
+        _gameManager = gameManager;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("UNO No Mercy API Working");
+    }
+
+    [HttpPost("create")]
+    public IActionResult CreateGame()
+    {
+        var deckService = new DeckService();
+
+        var deck = deckService.CreateDeck();
+
+        deckService.Shuffle(deck);
+
+        var game = new Game
+        {
+            Deck = deck,
+            Players =
+        {
+            new Player { Name = "Ashlin" },
+            new Player { Name = "Rahul" },
+            new Player { Name = "Arun" },
+            new Player { Name = "Vishnu" }
+        }
+        };
+
+        var gameService = new GameService();
+
+        gameService.DealCards(game);
+
+        Card startingCard;
+
+        while (true)
+        {
+            startingCard = gameService.DrawCard(game);
+
+            if (startingCard.Type == CardType.Number)
+            {
+                game.DiscardPile.Add(startingCard);
+                break;
+            }
+
+            game.Deck.Add(startingCard);
+
+            deckService.Shuffle(game.Deck);
+        }
+
+        _gameManager.CurrentGame = game;
+
+        return Ok(
+            gameService.GetGameState(game));
+    }
+
+    [HttpGet("state")]
+    public IActionResult GetState()
+    {
+        if (_gameManager.CurrentGame == null)
+        {
+            return NotFound(
+                "No active game found.");
+        }
+
+        var gameService = new GameService();
+
+        return Ok(
+            gameService.GetGameState(
+                _gameManager.CurrentGame));
+    }
+
+}
---
+++ UnoNoMercy.Api/Services/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnoNoMercy.GameEngine.Models;
+
+namespace UnoNoMercy.Api.Services
+{
+    public class GameManager
+    {
+        public Game? CurrentGame { get; set; }
+    }
+}

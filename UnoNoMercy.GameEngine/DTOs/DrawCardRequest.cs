3fd0b61bc7aace869bc40a1b21c1adf2e193780e
MNP-GH-411531 <411531@manappuram.com>
2026-08-12 17:03:53 +0530

    Added draw card request and response dto

---
+++ UnoNoMercy.GameEngine/DTOs/DrawCardRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UnoNoMercy.GameEngine.DTOs
+{
+    public class DrawCardRequest
+    {
+        public Guid GameId { get; set; }
+
+        public string PlayerName { get; set; }
+    }
+}
---
+++ UnoNoMercy.GameEngine/DTOs/DrawCardResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace UnoNoMercy.GameEngine.DTOs
+{
+    public class DrawCardResponse
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public string? DrawnCard { get; set; }
+    }
+}

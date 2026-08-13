8a06480c9615e95973a43e17138eda9965763f9c
MNP-GH-411531 <411531@manappuram.com>
2026-08-13 15:55:02 +0530

    Added room joining support to SignalR hub

--- UnoNoMercy.Api/Hubs/GameHub.cs
+++ UnoNoMercy.Api/Hubs/GameHub.cs
@@ -4,5 +4,12 @@ namespace UnoNoMercy.Api.Hubs
 {
     public class GameHub : Hub
     {
+        public async Task JoinRoom(
+            string roomCode)
+        {
+            await Groups.AddToGroupAsync(
+                Context.ConnectionId,
+                roomCode);
+        }
     }
 }
